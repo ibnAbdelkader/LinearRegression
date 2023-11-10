4d0ba3e98caf1dd5209db2268cbb6781c18cec07
Ahmed Abdelkader <ahmedqad7790@gmail.com>
2023-11-11 01:19:15 +0200

    Added correlation and used pandas for csv data extraction

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,5 +1,14 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+
+def Pearson_correlation(X,Y):
+    if len(X)==len(Y):
+        Sum_xy = sum((X-X.mean())*(Y-Y.mean()))
+        Sum_x_squared = sum((X-X.mean())**2)
+        Sum_y_squared = sum((Y-Y.mean())**2)       
+        corr = Sum_xy / np.sqrt(Sum_x_squared * Sum_y_squared)
+    return corr
  
 def estimate_coef(x, y):
     # number of observations/points
@@ -18,6 +27,7 @@ def estimate_coef(x, y):
     b_0 = m_y - b_1*m_x
  
     return (b_0, b_1)
+
  
 def plot_regression_line(x, y, b):
     # plotting the actual points as scatter plot
@@ -39,9 +49,13 @@ def plot_regression_line(x, y, b):
  
 def main():
     # observations / data
-    x = np.array([84.87882, 306.5947, 561.9845, 561.9845, 390.5684, 2175.03, 623.4731, 287.6025, 5512.038, 1783.18,405.2134,90.45606,492.2313,2469.645,1164.838,579.2083,292.9978,350.8515,368.1363,23.38284,2275.877,279.1726,1360.139,279.1726,480.6977,1487.868,383.8624,276.449])
-    y = np.array([37.9, 42.2, 47.3, 54.8, 43.1, 32.1, 40.3, 46.7, 18.8, 22.1,41.4,58.1,39.3,23.8,34.3,50.5,70.1,37.4,42.3,47.7,29.3,51.6,24.6,47.9,38.8,27,56.2,33.6])
- 
+    df = pd.read_csv("RealEstate.csv")
+    x = df.X3DistanceToTheNearestMRTStation
+    y = df.YHousePriceOfUnitArea
+    
+    #Check the correlation between predictor and response
+    print("Correlation: " ,Pearson_correlation(x,y))
+    
     # estimating coefficients
     b = estimate_coef(x, y)
     print("Estimated coefficients:\nb_0 = {}  \
